af9f63c7ade890ab321010ca5fe1d571fc06ca07
user <o.galvanabiega@gmail.com>
2024-09-17 14:23:54 +0200

    ASTEROIDEAK

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,6 +4,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class Movement : MonoBehaviour
 {
@@ -20,23 +22,23 @@ public class Movement : MonoBehaviour
     private float xySpeed;
     [SerializeField]
     private float lookSpeed;
-
     [SerializeField]
     private float forwardSpeed;
 
     [SerializeField]
     private Transform aimTarget;
+    [SerializeField]
+    TrailRenderer trailParticles;
+    [SerializeField]
+    TrailRenderer trailParticles2;
 
     [SerializeField]
-    ParticleSystem trailParticles;
+    private Transform cameraParent;
 
     private void Start()
     {
         playerModel = transform.GetChild(0);
         SetSpeed(forwardSpeed);
-
-
-
     }
     void Update()
     {
@@ -46,25 +48,21 @@ public class Movement : MonoBehaviour
         LocalMove(h, v, xySpeed);
         RotationLook(h, v, lookSpeed);
         HorizontalLean(playerModel, h, 40, .1f);
-
-        if(Input.GetButtonDown("BarrelRollLeft") || Input.GetButtonDown("BarrelRollRight"))
+        if (Input.GetButtonDown("BarrelRollLeft") || Input.GetButtonDown("BarrelRollRight"))
         {
-            
             int dir = Input.GetButtonDown("BarrelRollLeft") ? -1 : 1;
             QuickSpin(dir);
-
         }
-
-        if (Input.GetButtonDown("Boost")) ;
+        if (Input.GetButtonDown("Boost"))
         {
             Boost(true);
         }
-        if(Input.GetButtonUp("Boost"))
+        if (Input.GetButtonUp("Boost"))
         {
             Boost(false);
         }
     }
-
+    #region MovementLogic
     private void LocalMove(float x, float y, float speed)
     {
         transform.localPosition += new Vector3(x, y, 0) * speed * Time.deltaTime;
@@ -78,12 +76,12 @@ public class Movement : MonoBehaviour
         transform.position = Camera.main.ViewportToWorldPoint(pos);
     }
 
-
     void RotationLook(float h, float v, float speed)
     {
         aimTarget.parent.position = Vector3.zero;
         aimTarget.localPosition = new Vector3(h, v, 1);
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(aimTarget.position), Mathf.Deg2Rad * speed * Time.deltaTime);
+        gameObject.transform.rotation = Quaternion.RotateTowards(transform.rotation,
+            Quaternion.LookRotation(aimTarget.position, dollyCart.transform.up), Mathf.Deg2Rad * speed * Time.deltaTime);
     }
 
     void HorizontalLean(Transform target, float axis, float leanLimit, float lerpTime)
@@ -97,39 +95,71 @@ public class Movement : MonoBehaviour
     {
         dollyCart.m_Speed = speed;
     }
+    #endregion
 
+    #region Actions
     void QuickSpin(int dir)
     {
-        if(!DOTween.IsTweening(playerModel))
+        if (!DOTween.IsTweening(playerModel))
         {
-            playerModel.DOLocalRotate(new Vector3(playerModel.localEulerAngles.x, playerModel.localEulerAngles.y, -360 * dir), .4f, RotateMode.LocalAxisAdd).SetEase(Ease.OutSine);
-            //COLLIDER DESAKTIBATU
-            //PARTIKULA BATZUK AKTIBATU
+            playerModel.DOLocalRotate(new Vector3(playerModel.localEulerAngles.x, playerModel.localEulerAngles.y, 360 * -dir), .4f,
+                RotateMode.LocalAxisAdd).SetEase(Ease.OutSine);
+            //Collider desaktibatu
+            //Partikula batzuk aktibatu 
+
         }
     }
 
-    void Boost (bool state)
+    void Boost(bool state)
     {
-        if(state)
+        /*if (state)
         {
             trailParticles.Play();
         }
         else
         {
             trailParticles.Stop();
-        }
-        trailParticles.GetComponent<TrailRenderer>().emitting = state;
+        }*/
+        trailParticles.emitting = state;
+        trailParticles2.emitting = state;
+
+        float zoom = state ? -10 : 0;
+
+        float originChrom = state ? 0 : 1;
+        float endChrom = state ? 1 : 0;
+
+        float originFieldOfView = state ? 40 : 40;
+        float endFieldOfView = state ? 40 : 40;
 
         float speed = state ? forwardSpeed * 2 : forwardSpeed;
 
         DOVirtual.Float(dollyCart.m_Speed, speed, .15f, SetSpeed);
+        DOVirtual.Float(originChrom, endChrom, .5f, Chromatic);
+        DOVirtual.Float(originFieldOfView, endFieldOfView, .5f, FieldOfView);
 
+        SetCameraZoom(zoom, .4f);
     }
+    #endregion
+
+    #region PostProcessing
+    void SetCameraZoom(float zoom, float duration)
+    {
+        cameraParent.DOLocalMove(new Vector3(0, 0, zoom), duration);
+    }
+    void FieldOfView(float fov)
+    {
+        cameraParent.GetComponentInChildren<CinemachineVirtualCamera>().m_Lens.FieldOfView = fov;
+    }
+    void Chromatic(float x)
+    {
+        Camera.main.GetComponent<Volume>().profile.TryGet(out ChromaticAberration c);
+        c.intensity.value = x;
+    }
+    #endregion
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.blue;
         Gizmos.DrawSphere(aimTarget.position, .15f);
         Gizmos.DrawWireSphere(aimTarget.position, .5f);
-       
     }
 }
